8d67cfb8c5360577ad238148645d8c1cce7f75aa
zhangxin <2916733574@qq.com>
2024-09-07 10:20:23 +0800

    网络

--- base/http/server/main.go
+++ base/http/server/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
-	"io/ioutil"
 )
 
-func home(w http.ResponseWriter,r *http.Request){
-   str := "hello"
-   w.Write([]byte(str))
+func home(w http.ResponseWriter, r *http.Request) {
+	str := "hello"
+	w.Write([]byte(str))
 }
-func getUrl(w http.ResponseWriter,r *http.Request){
+func getUrl(w http.ResponseWriter, r *http.Request) {
 	// 处理客户端的请求
 	fmt.Println(r.URL)
 	fmt.Println(r.Method)
@@ -23,27 +23,27 @@ func getUrl(w http.ResponseWriter,r *http.Request){
 	// 发送数据给客户端
 	w.Write([]byte("ok"))
 }
-func postUrl(w http.ResponseWriter,r *http.Request){
+func postUrl(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	// 1. 请求类型是application/x-www-form-urlencoded时解析form数据
 	r.ParseForm()
-	fmt.Println(r.PostForm) 
+	fmt.Println(r.PostForm)
 	fmt.Println(r.PostForm.Get("name"), r.PostForm.Get("age"))
 	// 2. 请求类型是application/json时从r.Body读取数据
-	b,err := ioutil.ReadAll(r.Body)
-	if err != nil{
-		fmt.Printf("read request.body err:%v",err)
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("read request.body err:%v", err)
 		return
 	}
 	fmt.Println(string(b))
 	answer := `{"status": "ok"}`
 	w.Write([]byte(answer))
 }
-func main(){
-	http.HandleFunc("/",home)
-	http.HandleFunc("/userinfo",getUrl)
-	http.HandleFunc("/list",postUrl)
-	err := http.ListenAndServe("127.0.0.1:20000",nil)
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/userinfo", getUrl)
+	http.HandleFunc("/list", postUrl)
+	err := http.ListenAndServe("127.0.0.1:20000", nil)
 	if err != nil {
 		fmt.Printf("http server failed, err:%v\n", err)
 		return
--- github-old/base/testpackage/testpackage.go
+++
@@ -1,7 +0,0 @@
-package testpackage
-
-import "fmt"
-
-func Test() {
-	fmt.Println("我是testpackage包的内容")
-}
