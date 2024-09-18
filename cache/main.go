a1333601253ea406c5f33daaf453e905406f6ab5
zhangxin <2916733574@qq.com>
2024-09-18 17:30:03 +0800

    单机并发缓存

--- cache/main.go
+++ cache/main.go
@@ -1,26 +1,103 @@
 package main
 
 import (
-	"golang-study/cache/lru"
+	"fmt"
+	"log"
+	"sync"
 )
 
-type String string
-
-func (d String) Len() int {
-	return len(d)
+type Getter interface {
+	Get(key string) ([]byte, error)
 }
 
-// 分布式缓存
-func main() {
-	keys := make([]string, 0)
-	callback := func(key string, value lru.Value) {
-		keys = append(keys, key)
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups map[string]*Group
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil getter")
 	}
-	l := lru.NewCache(int64(10), callback)
-	l.Add("key1", String("123"))
-	l.Add("k2", String("234"))
-	l.Add("k3", String("345"))
-	l.Add("k4", String("456"))
-	l.ShowList()
-
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
 }
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	g := groups[name]
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
+
+// type String string
+
+// func (d String) Len() int {
+// 	return len(d)
+// }
+
+// // 分布式缓存
+// func main() {
+// 	keys := make([]string, 0)
+// 	callback := func(key string, value lru.Value) {
+// 		keys = append(keys, key)
+// 	}
+// 	l := lru.NewCache(int64(10), callback)
+// 	l.Add("key1", String("123"))
+// 	l.Add("k2", String("234"))
+// 	l.Add("k3", String("345"))
+// 	l.Add("k4", String("456"))
+// 	l.ShowList()
+
+// }
